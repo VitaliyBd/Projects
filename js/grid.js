3e20eadf34bddd86920b9fd496e9e058339d5008
VitaliyBd <125589442+VitaliyBd@users.noreply.github.com>
2025-03-09 15:23:54 -0400

    Created js/grid.js for the grid effect

---
+++ js/grid.js
@@ -0,0 +1,7 @@
+// This file will implement the grid generator expect
+function initGrid() {
+    console.log("Generating grid effect");
+    // This will contain the logic for rendering the grid
+}
+
+export { initGrid };
